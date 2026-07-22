895b3f8c730614abdb6d3e21d6bc672e6d32bd5e
SparroHawc <sparrohawc@gmail.com>
2026-07-21 19:16:38 -0700

    Add elerium tracking

--- Profit.cs
+++ Profit.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Text;
+using System.Diagnostics;
 
 
 namespace ParseTextXcom
@@ -13,152 +14,156 @@ namespace ParseTextXcom
         static void Main(string[] args)
         {
             StreamReader manufacture = new StreamReader(@"user\mods\XComFiles\Ruleset\manufacture_XCOMFILES.rul");
+            StreamReader baseManufacture = new StreamReader(@"standard\xcom1\manufacture.rul");
             StreamReader items = new StreamReader(@"user\mods\XComFiles\Ruleset\items_XCOMFILES.rul");
+            StreamReader baseItems = new StreamReader(@"standard\xcom1\items.rul");
             StreamReader modEnUs = new StreamReader(@"user\mods\XComFiles\Language\en-US.yml");
             StreamReader baseEnUs = new StreamReader(@"standard\xcom1\Language\en-US.yml");
+
+            StreamReader[] manufactureReaders = { manufacture, baseManufacture };
+            StreamReader[] itemsReaders = { items, baseItems };
+            StreamReader[] langReaders = { modEnUs, baseEnUs };
 
             var itemList = new List<Item>();
             var sellList = new List<SellPrice>();
 
+            string testLine;
+
             //Pull the sell price data
-            string testLine = items.ReadLine().Trim();
+            foreach (StreamReader itemsReader in itemsReaders)
+            { 
+                testLine = itemsReader.ReadLine().Trim();
 
-            while (!items.EndOfStream)
-            {
-                //get name of item
-                if (Regex.IsMatch(testLine, "- type: .*"))
+                while (!itemsReader.EndOfStream)
                 {
-                    var itemName = testLine.Split(' ').Last();
-
-                    //find sell price
-                    testLine = items.ReadLine().Trim();
-                    while (!items.EndOfStream && !Regex.IsMatch(testLine, "- type: .*"))
+                    //get name of item
+                    if (Regex.IsMatch(testLine, "- type: .*"))
                     {
-                        if (Regex.IsMatch(testLine, "costSell: .*"))
-                        {
-                            SellPrice tempSellPrice = new SellPrice()
-                            {
-                                name = itemName,
-                                price = int.Parse(testLine.Split(' ').Last())
-                            };
-                            sellList.Add(tempSellPrice); // Only add if we have everything
-                        }
-                        testLine = items.ReadLine().Trim();
-                    }
+                        var itemName = testLine.Split(' ').Last();
 
-                }
-                else
-                {
-                    testLine = items.ReadLine().Trim();
+                        //find sell price
+                        testLine = itemsReader.ReadLine().Split('#').First().Trim();
+                        while (!itemsReader.EndOfStream && !Regex.IsMatch(testLine, "- type: .*"))
+                        {
+                            if (Regex.IsMatch(testLine, "costSell: .*"))
+                            {
+                                SellPrice tempSellPrice = new SellPrice()
+                                {
+                                    name = itemName,
+                                    price = int.Parse(testLine.Split(' ').Last())
+                                };
+                                sellList.Add(tempSellPrice); // Only add if we have everything
+                            }
+                            testLine = itemsReader.ReadLine().Split('#').First().Trim();
+                        }
+
+                    }
+                    else
+                    {
+                        testLine = itemsReader.ReadLine().Trim();
+                    }
                 }
             }
 
             //Build the localization database
             Dictionary<string, string> localization = new Dictionary<string, string>();
-            testLine = modEnUs.ReadLine().Trim();
-            while (!modEnUs.EndOfStream)
-            {
-                if (Regex.IsMatch(testLine, "STR_.*"))
+            foreach (StreamReader langReader in langReaders)
+            { 
+                testLine = langReader.ReadLine().Trim();
+                while (!langReader.EndOfStream)
                 {
-                    var key = testLine.Split(':').First();
-                    var value = testLine.Split(':').Last().Trim();
-                    if(!localization.ContainsKey(key)) localization.Add(key, value);
+                    if (Regex.IsMatch(testLine, "STR_.*"))
+                    {
+                        var key = testLine.Split(':').First();
+                        var value = testLine.Split(':').Last().Trim();
+                        if (!localization.ContainsKey(key)) localization.Add(key, value);
+                    }
+                    testLine = langReader.ReadLine().Trim();
                 }
-                testLine = modEnUs.ReadLine().Trim();
-            }
-
-            testLine = baseEnUs.ReadLine().Trim();
-            while (!baseEnUs.EndOfStream)
-            {
-                if (Regex.IsMatch(testLine, "STR_.*"))
-                {
-                    var key = testLine.Split(':').First();
-                    var value = testLine.Split(':').Last().Trim();
-                    if (!localization.ContainsKey(key)) localization.Add(key, value);
-                }
-                testLine = baseEnUs.ReadLine().Trim();
             }
 
             //Now pull the manufacturing data
-            testLine = manufacture.ReadLine().Trim();
-
-            while (!manufacture.EndOfStream)
+            foreach(StreamReader manufactureReader in manufactureReaders)
             {
-                if (Regex.IsMatch(testLine, "- name: .*"))
+                testLine = manufactureReader.ReadLine().Trim();
+
+                while (!manufactureReader.EndOfStream)
                 {
-                    localization.TryGetValue(testLine.Split(' ').Last(), out string tempLocalName);
-
-                    Item currItem = new Item
+                    if (Regex.IsMatch(testLine, "- name: .*"))
                     {
-                        name = testLine.Split(' ').Last(),
-                        localName = tempLocalName
-                    };
-                    if (currItem.localName == null)
-                    {
-                        currItem.localName = currItem.name;
-                    }
+                        localization.TryGetValue(testLine.Split(' ').Last(), out string tempLocalName);
 
-                    currItem.extraMats = new List<ExtraMat>();
+                        Item currItem = new Item
+                        {
+                            name = testLine.Split(' ').Last(),
+                            localName = tempLocalName
+                        };
+                        if (currItem.localName == null)
+                        {
+                            currItem.localName = currItem.name;
+                        }
 
-                    testLine = manufacture.ReadLine().Trim();
-                    while (!manufacture.EndOfStream && !Regex.IsMatch(testLine, "- name: .*"))
-                    {
-                        if (Regex.IsMatch(testLine, "space: .*"))
+                        currItem.extraMats = new List<ExtraMat>();
+
+                        testLine = manufactureReader.ReadLine().Trim();
+                        while (!manufactureReader.EndOfStream && !Regex.IsMatch(testLine, "- name: .*"))
                         {
-                            currItem.space = int.Parse(testLine.Split(' ').Last());
-                            currItem.TotalSpace = currItem.space;
-                        }
-                        else if (Regex.IsMatch(testLine, "time: .*"))
-                        {
-                            currItem.time = int.Parse(testLine.Split(' ').Last());
-                            currItem.TotalTime = currItem.time;
-                        }
-                        else if (Regex.IsMatch(testLine, "cost: .*"))
-                        {
-                            currItem.cost = int.Parse(testLine.Split(' ').Last());
-                            currItem.TotalCost = currItem.cost;
-                        }
-                        else if (Regex.IsMatch(testLine, "requiredItems:"))
-                        {
-                            // Add in manufacturing components
-                            testLine = manufacture.ReadLine().Trim();
-                            while (Regex.IsMatch(testLine, "STR_.*"))
+                            if (Regex.IsMatch(testLine, "space: .*"))
                             {
-                                var tempSplit = testLine.Split(' ');
-                                // skip anything without a quantity
-                                if(Regex.IsMatch(tempSplit.Last(), "\\d+"))
+                                currItem.space = int.Parse(testLine.Split(' ').Last());
+                                currItem.TotalSpace = currItem.space;
+                            }
+                            else if (Regex.IsMatch(testLine, "time: .*"))
+                            {
+                                currItem.time = int.Parse(testLine.Split(' ').Last());
+                                currItem.TotalTime = currItem.time;
+                            }
+                            else if (Regex.IsMatch(testLine, "cost: .*"))
+                            {
+                                currItem.cost = int.Parse(testLine.Split(' ').Last());
+                                currItem.TotalCost = currItem.cost;
+                            }
+                            else if (Regex.IsMatch(testLine, "requiredItems:"))
+                            {
+                                // Add in manufacturing components
+                                testLine = manufactureReader.ReadLine().Trim();
+                                while (!manufactureReader.EndOfStream && Regex.IsMatch(testLine, "STR_.*"))
                                 {
-                                    ExtraMat extraMat = new ExtraMat()
+                                    var tempSplit = testLine.Split(' ');
+                                    // skip anything without a quantity
+                                    if (Regex.IsMatch(tempSplit.Last(), "\\d+"))
+                                    {
+                                        ExtraMat extraMat = new ExtraMat()
                                         { name = tempSplit.First(), qty = int.Parse(tempSplit.Last()) };
 
-                                    currItem.extraMats.Add(extraMat);
+                                        currItem.extraMats.Add(extraMat);
+                                    }
+                                    testLine = manufactureReader.ReadLine().Trim();
                                 }
-                                testLine = manufacture.ReadLine().Trim();
                             }
+
+                            if(!manufactureReader.EndOfStream) testLine = manufactureReader.ReadLine().Trim();
+                        }
+                        var rtnList = sellList.Where(x => x.name == currItem.name).ToArray();
+                        if (rtnList.Length > 0)
+                        {
+                            currItem.sellPrice = rtnList.First().price;
+                            itemList.Add(currItem);
                         }
 
-                        testLine = manufacture.ReadLine().Trim();
                     }
-                    var rtnList = sellList.Where(x => x.name == currItem.name).ToArray();
-                    if (rtnList.Length > 0)
-                    {
-                        currItem.sellPrice = rtnList.First().price;
-                        itemList.Add(currItem);
-                    }
+
+                    else if (!manufactureReader.EndOfStream)
+                        testLine = manufactureReader.ReadLine().Trim();
 
                 }
-
-                else if (!manufacture.EndOfStream)
-                    testLine = manufacture.ReadLine().Trim();
-
             }
-
             // At this point, we have a valid array of items, but without the
             // buildable materials costs factored in.  Let's fix that.
 
             bool matsClearedFlag = false;
-            while(!matsClearedFlag)
-            { 
+            while (!matsClearedFlag)
+            {
                 matsClearedFlag = true;
 
                 // Pick out the things that have extra mats so we can iterate through them
@@ -167,21 +172,26 @@ namespace ParseTextXcom
                 {
                     // Go through each mat and check if the item it points to is done being calculated; if so, add its totals.  Otherwise pass over it this loop. 
                     Item currItem = subList[itemIdx];
-                    for(int matIdx = currItem.extraMats.Count - 1; matIdx >= 0; matIdx--)
+
+                    for (int matIdx = currItem.extraMats.Count - 1; matIdx >= 0; matIdx--)
                     {
                         ExtraMat currMat = currItem.extraMats[matIdx];
                         Item matsItem = itemList.Find(x => x.name == currMat.name.Trim(':'));
                         //Kick it out if we can't manufacture it
-                        if(matsItem == null)
-                        { 
-                            //TODO: Elerium
+                        if (matsItem == null)
+                        {
+                            //Add up elerium cost
+                            if (currMat.name.Trim(':') == "STR_ELERIUM_115")
+                            {
+                                currItem.elerium = currMat.qty;
+                            }
                             currItem.other = true;
                             currItem.extraMats.Remove(currMat);
                             continue;
                         }
                         // Not everything in that mat is calculated, so we set the loop to go again and skip it
-                        if(matsItem.extraMats.Count > 0)
-                        { 
+                        if (matsItem.extraMats.Count > 0)
+                        {
                             matsClearedFlag = false;
                             continue;
                         }
@@ -194,8 +204,9 @@ namespace ParseTextXcom
                         currItem.TotalSpace += matsItem.TotalSpace;
                         currItem.alloys += matsItem.alloys * currMat.qty;
                         currItem.elerium += matsItem.elerium * currMat.qty;
+                        if(matsItem.other) currItem.other = true;
                         currItem.extraMats.Remove(currMat);
-                        
+
                     }
                 }
             }
@@ -212,7 +223,7 @@ namespace ParseTextXcom
                 }
             }
 
-                       // Console.ReadKey();
+            // Console.ReadKey();
         }
     }
 
@@ -231,7 +242,7 @@ namespace ParseTextXcom
         public int TotalCost { get; set; }
         public int TotalTime { get; set; }
         public int TotalSpace { get; set; }
-        
+
         public List<ExtraMat> extraMats;
     }
 
